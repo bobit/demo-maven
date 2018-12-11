8be3d06353d8b1f67e3e759db251a13d4b34129c
zhangb <iloveapi@163.com>
2018-12-11 23:24:09 +0800

    update

--- demo-interview/src/main/java/SignatureExample.java
+++ demo-interview/src/main/java/SignatureExample.java
@@ -1,7 +1,11 @@
+import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin;
+
 import javax.xml.bind.DatatypeConverter;
 import java.security.*;
 import java.security.interfaces.DSAPrivateKey;
 import java.security.interfaces.DSAPublicKey;
+import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
@@ -18,8 +22,10 @@ public class SignatureExample {
     private static String src = "hello";
 
     public static void main(String[] args) {
-        jdkDSA();
+//        jdkDSA();
+        jdkECDSA();
     }
+
 
     public static void jdkDSA() {
         // 1. 初始化 秘钥,生成DSA算法的公钥和私钥
@@ -59,4 +65,36 @@ public class SignatureExample {
             e.printStackTrace();
         }
     }
+
+    public static void jdkECDSA() {
+        // 1. 初始化 密钥
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+            keyPairGenerator.initialize(256);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            ECPublicKey ecPublicKey = (ECPublicKey) keyPair.getPublic();
+            ECPrivateKey ecPrivateKey = (ECPrivateKey) keyPair.getPrivate();
+            // 2. 执行签名
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(ecPrivateKey.getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance("EC");
+            PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+            Signature signature = Signature.getInstance("SHA1withECDSA");
+            signature.initSign(privateKey);
+            signature.update(src.getBytes());
+            byte[] arr = signature.sign();
+            System.out.println("jdk ecdsa sign :" + HexBin.encode(arr));
+            // 3. 验证签名
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(ecPublicKey.getEncoded());
+            keyFactory = KeyFactory.getInstance("EC");
+            PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+            signature = Signature.getInstance("SHA1withECDSA");
+            signature.initVerify(publicKey);
+            signature.update(src.getBytes());
+            boolean bool = signature.verify(arr);
+            System.out.println("jdk ecdsa verify:" + bool);
+        } catch (Exception e) {
+
+        }
+    }
+
 }
