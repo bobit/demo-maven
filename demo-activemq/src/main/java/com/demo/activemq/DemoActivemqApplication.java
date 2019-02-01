a04fa0df713a0aa3c7cf868025ca7be8ef36c940
zhangb <iloveapi@163.com>
2019-02-01 23:46:55 +0800

    update

--- demo-activemq/src/main/java/com/demo/activemq/DemoActivemqApplication.java
+++ demo-activemq/src/main/java/com/demo/activemq/DemoActivemqApplication.java
@@ -1,4 +1,4 @@
-package com.example.demoactivemq;
+package com.demo.activemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- demo-activemq/src/test/java/com/demo/activemq/DemoActivemqApplicationTests.java
+++ demo-activemq/src/test/java/com/demo/activemq/DemoActivemqApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demoactivemq;
+package com.demo.activemq;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
